333099fb06b993ff6cb8fa280d20fe33ae5c0338
kadirovaMadina <madina.80008@gmail.com>
2024-04-04 10:15:30 +0500

    Добавьте файлы проекта.

---
+++ Domain/Entities/Entrance.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enum;
+
+namespace Domain.Entities
+{
+    public class Entrance
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public int NumberFloor { get; set; }
+        public int NumberApartmentPerFloor { get; set; }
+        public bool HasLift { get; set; }
+        public Building Building { get; set; }
+        public Guid BuildingId { get; set; }
+        public virtual ICollection<Apartment> Apartments { get; set; }
+    }
+}
